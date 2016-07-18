cb74eca8ab82a16fe495d43851f6389c8523663c
Syd Polk <sydpolk@gmail.com>
2016-07-17 21:36:32 -0500

    Stupid top-level page. This page won't get linked anywhere, but for now...

--- bjsvc/urls.py
+++ bjsvc/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import mainviews
+
 urlpatterns = [
+    url(r'^$', mainviews.main_index_view, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
